97b8b0e53205acd75c9d5148840b14df3defef94
iknek <imad.alihodzic@yahoo.com>
2021-09-09 13:05:24 +0200

    Start with just a window opening
    Used to verifuy libgdx is installed properly.

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.TopDownBois;
 
+
+//Pasted directly from libgdx start page
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new TopDownBois(), config);
 	}
 }
