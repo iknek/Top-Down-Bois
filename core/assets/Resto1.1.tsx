0a6007b9b7e8ecb4092818f29e4e324130cf1c7b
iknek <imad.alihodzic@yahoo.com>
2021-09-11 16:44:37 +0200

    Added game assets + class for displaying tiled maps. See desc. for more info
    2 self made maps have been added, and their associated tilepacks which have been found online.

---
+++ core/assets/Resto1.1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Resto1.1" tilewidth="16" tileheight="16" tilecount="735" columns="35">
+ <image source="Resto2.0/Resto1.1.png" width="560" height="336"/>
+</tileset>
--- desktop/src/com/mygdx/game/desktop/Main.java
+++ desktop/src/com/mygdx/game/desktop/Main.java
@@ -9,10 +9,12 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "hello-world";
-        //cfg.useGL20 = false; //should this really be not commented?....
-        cfg.width = 480;
-        cfg.height = 320;
+        //cfg.useGL20 = false; //should this really be commented out?....
+        cfg.width = 1920;
+        cfg.height = 1080;
 
-        new LwjglApplication(new DesktopLauncher(), cfg);
+       // new LwjglApplication(new DesktopLauncher(), cfg);
+        new LwjglApplication(new TiledTest(), cfg);
+
     }
 }
