59f528114b64222f5e421bc534b96784f499f18a
iknek <imad.alihodzic@yahoo.com>
2021-09-23 16:01:15 +0200

    More textures needed.

---
+++ core/assets/Bush-4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Bush-4" tilewidth="28" tileheight="19" tilecount="1" columns="1">
+ <image source="Small Forest Asset Pack/Bushes/Bush-4.png" width="28" height="19"/>
+</tileset>
