cc7f2df50954b6c48c6734404088fd9c0e058ab8
iknek <imad.alihodzic@yahoo.com>
2021-09-23 16:44:52 +0200

    Added spwawn details to the map in the form of layers, and associated map textures Authors: David & Imad

---
+++ core/assets/Earth-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Earth-tileset" tilewidth="16" tileheight="16" tilecount="60" columns="12">
+ <image source="Small Forest Asset Pack/Ground tileset/Earth-tileset.png" width="206" height="92"/>
+</tileset>
---
+++ core/assets/Small Forest Asset Pack/Stone-path-tileset-vertical.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Stone-path-tileset-vertical" tilewidth="37" tileheight="182" tilecount="1" columns="1">
+ <image source="Ground tileset/Stone-path-tileset-vertical.png" width="37" height="182"/>
+</tileset>
