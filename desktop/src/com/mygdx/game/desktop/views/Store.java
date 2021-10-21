0a6e471b4c8f4ecf90950aea9b593aa406575cf5
david-ferm00 <david@ferm.co.uk>
2021-10-21 11:35:15 +0200

    Testing shop stuff Author: David

--- desktop/src/com/mygdx/game/desktop/views/Store.java
+++ desktop/src/com/mygdx/game/desktop/views/Store.java
@@ -42,24 +42,17 @@ public class Store implements Disposable{
         stage = new Stage(viewport, sb);
         Gdx.input.setInputProcessor(stage);
 
-        Table table = new Table();
-        table.center();
-        table.setWidth(640*scale);
-        //table.setHeight(640*scale);
-        table.scaleBy(scale);
-
-        // Pos of table, size, and alignment
-        table.setPosition(0,320);
-        table.align(Align.center);
+        final Stack stack = new Stack();
 
         //Defines images
         storeImage = new Image(new Texture(Gdx.files.internal("Store/store2.png")));
-        table.add(storeImage).size(540,540);
+        stack.add(storeImage);
 
         //Button skins for when clicked and not clicked
-        TextureRegion button_down = new TextureRegion(new Texture(Gdx.files.internal("Store/buttonDown.png")));
+        /*TextureRegion button_down = new TextureRegion(new Texture(Gdx.files.internal("Store/buttonDown.png")));
         TextureRegion button_up = new TextureRegion(new Texture(Gdx.files.internal("Store/buttonUp.png")));
         skin = new Skin();
+        skin.setScale((scale*9)/20);
 
         skin.add("buttonDown", button_down);
         skin.add("buttonUp", button_up);
@@ -69,22 +62,54 @@ public class Store implements Disposable{
         buttonStyle.up = skin.getDrawable("buttonUp");
         buttonStyle.down = skin.getDrawable("buttonDown");
 
+        Table buttonTable = new Table();
+        buttonTable.left();
+        buttonTable.padLeft(42*scale);
+        buttonTable.top();
+        buttonTable.padTop(240*scale);
         //Creates and adds button to table
         button = new Button(buttonStyle);
+        buttonTable.add(button);
 
-        //Listener for click
-        button.addListener(new ChangeListener() {
+        stack.add(buttonTable);*/
+
+        final Table table = new Table();
+        table.center();
+        table.setWidth(640*scale);
+        //table.setHeight(640*scale);
+        table.scaleBy(scale);
+
+        // Pos of table, size, and alignment
+        table.setPosition(0,320);
+        table.align(Align.center);
+
+        table.addListener(new ClickListener(){
             @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                Gdx.app.exit();
+            public void clicked(InputEvent event, float x, float y){
+                int row = -1;
+                int col = -1;
+                if (y>5*scale && y<15*scale){
+                    row=0;
+                } else if(y<-46.5*scale && y>-56.5*scale){
+                    row=1;
+                } else if(y<-106.5*scale && y>-116.5*scale){
+                    row=2;
+                }
+                if (x>42*scale && x<(42 + 50)*scale){
+                    col=0;
+                } else if(x>138*scale && x<183*scale){
+                    col=1;
+                } else if(x>230*scale && x<275*scale){
+                    col=2;
+                }
+                System.out.println(row + " " + col);
+                buy(row*3 + col);
             }
         });
 
-        table.row();
-        table.add(button);
+        table.add(storeImage).size(540,540);
         //Adds table to stage
         stage.addActor(table);
-
     }
 
     public void update(){
@@ -97,5 +122,31 @@ public class Store implements Disposable{
     @Override
     public void dispose(){
         stage.dispose();
+    }
+
+    private void buy(int index){
+        System.out.println(index);
+        switch(index){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            default:
+                break;
+        }
     }
 }
